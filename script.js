3da1cb8871f2b46f56e80f95a408175352fa6950
skrishna-code <krishnasutar026@gmail.com>
2026-09-02 07:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	let check="/^[\w.-]+@[\w-]+(?:\.[\w-]+)+$/";
+	 return check.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
